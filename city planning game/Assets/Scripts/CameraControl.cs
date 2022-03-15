b5b3e6148928f638029a796f012c9d956ef711c6
Varun Chadha <vchadha023@gmail.com>
2022-03-15 10:34:27 -0500

    Add basic rotational behaviour
    Added ability for camera to rotate around vertical axis at zero
    or around speicifed target object.
    We do not update camera position or offset if target object moves
    since we assume a static world at the moment.

--- city planning game/Assets/Scripts/CameraControl.cs
+++ city planning game/Assets/Scripts/CameraControl.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {    
-    private float horizontalInput;
+    // TODO: change to game object first then get transform from object?
+    public Transform target;
+    public Transform cameraPos;
+
+    public float X, Y, Z;
+    public float angularSpeed = 100f;
+
+    Vector3 cameraOffset;
+    Vector3 targetPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set camera offset from object
+        cameraOffset = new Vector3( 0, 5, -8 );
+
+        // Get target position to rotate around
+        if ( target == null ) {
+            targetPos = Vector3.zero;
+        } else {
+            targetPos = target.position;
+        }
+
+        // Set transform rotation and position
+        transform.rotation = Quaternion.Euler( 15, 0, 0 );
+        transform.position = targetPos + cameraOffset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = 2 * Input.GetAxis("Horizontal");
     }
 
-    void FixedUpdate() { //physics update - 100hz
-        transform.RotateAround(Vector3.zero, Vector3.up, horizontalInput);
+    void LateUpdate() {
+        float horizontalInput = Input.GetAxis( "Horizontal" ) * angularSpeed * Time.deltaTime;
+
+        // We have moved significantly
+        if ( !Mathf.Approximately( horizontalInput, 0f ) ) {
+            transform.RotateAround( targetPos, Vector3.up, horizontalInput );
+        }
     }
-        
 }
