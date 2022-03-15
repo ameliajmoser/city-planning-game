3875f6affcfd0245ff3f0c8ae875329af538f6f9
Varun Chadha <vchadha023@gmail.com>
2022-03-15 11:36:09 -0500

    Add zoom function
    Changed zoom to update camera offset.

--- city planning game/Assets/Scripts/CameraControl.cs
+++ city planning game/Assets/Scripts/CameraControl.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {    
-    // TODO: change to game object first then get transform from object?
+    // Transforms for determining rotation and position
     public Transform target;
     public Transform cameraPos;
 
+    // Camera relative position and speed
     public float X, Y, Z;
     public float angularSpeed = 100f;
 
+    // Camera offset and target vectors
     Vector3 cameraOffset;
+    Vector3 zoomOffset;
     Vector3 targetPos;
 
     // Start is called before the first frame update
@@ -35,14 +38,36 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Set relative location
+        X = cameraPos.position.x - targetPos.x;
+        Y = cameraPos.position.y - targetPos.y;
+        Z = cameraPos.position.z - targetPos.z;
+
+        // Set zoom offset
+        zoomOffset = new Vector3( ( X / 10 ), ( Y / 10 ), ( Z / 10 ) );
+
+        // Get scroll input and update offset
+        float mouseScroll = Input.GetAxis( "Mouse ScrollWheel" );
+
+        if ( mouseScroll > 0 ) {
+            cameraOffset -= zoomOffset;
+        } else if ( mouseScroll < 0 ) {
+            cameraOffset += zoomOffset;
+        }
+        
+        // Update camera position
+        transform.position = targetPos + cameraOffset;
     }
 
     void LateUpdate() {
+        // Get horizontal input
         float horizontalInput = Input.GetAxis( "Horizontal" ) * angularSpeed * Time.deltaTime;
 
         // We have moved significantly
         if ( !Mathf.Approximately( horizontalInput, 0f ) ) {
+            // Update rotation and camera offset
             transform.RotateAround( targetPos, Vector3.up, horizontalInput );
+            cameraOffset = transform.position - targetPos;
         }
     }
 }
