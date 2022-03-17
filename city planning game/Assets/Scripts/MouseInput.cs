5ad875094969111ddbbed80cb37da90dfd4591cc
Amelia Jade Moser <tpmoser@wisc.edu>
2022-03-17 14:09:45 -0500

    basic mouse input
    click to place a cube and print the coordinates to console
    
    currently does not have a system for avoiding UI element collision - this will need to be added later

---
+++ city planning game/Assets/Scripts/MouseInput.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseInput : MonoBehaviour
+{
+    public Action<Vector3> OnMouseDown, OnMouseHold;
+    public Action OnMouseUp;
+    public LayerMask groundMask;
+
+    [SerializeField]
+    Camera mainCamera;
+
+    private void Update(){
+        CheckClickDownEvent();
+        CheckClickUpEvent();
+        CheckClickHoldEvent();
+    }
+
+    private Vector3? RaycastGround(){
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, groundMask)){
+            return hit.point;
+        }
+        return null;
+    }
+    private void CheckClickHoldEvent()
+    {
+        if(Input.GetMouseButton(0)){
+            var pos = RaycastGround();
+            if (pos != null){
+                OnMouseHold?.Invoke(pos.Value);
+                //Debug.Log("Hold: " + pos.Value);
+            }
+        }
+    }
+
+    private void CheckClickUpEvent()
+    {
+        if(Input.GetMouseButtonUp(0)){
+            OnMouseUp?.Invoke();
+        }
+
+    }
+
+    private void CheckClickDownEvent()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            var pos = RaycastGround();
+            if (pos != null){
+                OnMouseDown?.Invoke(pos.Value);
+                //Debug.Log("Down: " + pos.Value);
+            }
+        }
+    }
+}
